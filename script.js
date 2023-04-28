f9750b5dc2e7f835cbdea34e92d0f423501d074b
Your Name <you@example.com>
2023-04-28 17:41:50 -0300

    1 up

---
+++ script.js
@@ -0,0 +1,16 @@
+// let dashboard = document.querySelector('aside')
+// let btnMob = document.querySelector('.hamburger-icon')
+
+
+// btnMob.addEventListener("click",()=>{
+//     if(dashboard.id == 'true'){
+//         dashboard.id = 'false'
+//         dashboard.style.transform = 'translateX(0px)'
+//         btnMob.style.transform = 'translate(200px, 50px)'
+//     }
+//     else{
+//         dashboard.id = 'true'
+//         dashboard.style.transform = 'translateX(-350px)'
+//         btnMob.style.transform = 'translate(0)'
+//     }
+// })
